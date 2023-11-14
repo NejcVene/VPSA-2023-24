bb7fc4ea514930b0b4298e2b897a72cdf0aaf530
NejcVene <nejcvene.nv@gmail.com>
2023-11-14 16:13:02 +0100

    Added serial Amicable number search

--- Predavanja/Predavanje6/prvi.c
+++ Predavanja/Predavanje6/prvi.c
@@ -5,19 +5,27 @@
 #include <math.h>
 #include <omp.h>
 
-#define N 1000000
+#define N 6500
 
-int vsotaDeljiteljev(int);
+int vsotaDeliteljevFunc(int);
+
+int vsoteDeliteljev[N];
 
 int main(void) {
 
-    int sumX = 0, sumY = 0;
+    int sdi;
     for (int x = 1; x<N; x++) {
-        sumX = vsotaDeljiteljev(x);
-        for (int j = x + 1; j<x; j++) {
-            sumY = vsotaDeljiteljev(j);
-            if (x == sumY && j == sumX) {
-                // is a friend
+        vsoteDeliteljev[x] = vsotaDeliteljevFunc(x);
+        printf("Vsota deliteljev števila %d je %d\n", x, vsoteDeliteljev[x]);
+    }
+
+    for (int i = 1; i<N; i++) {
+        sdi = vsoteDeliteljev[i];
+        if (sdi < N) {
+            if (i < sdi) {
+                if (vsoteDeliteljev[sdi] == i) {
+                    printf("Prijatelski števili: %d %d\n", i, sdi);
+                }
             }
         }
     }
@@ -26,12 +34,15 @@ int main(void) {
 
 }
 
-int vsotaDeljiteljev(int x) {
+int vsotaDeliteljevFunc(int x) {
 
-    int vsota = 0;
-    for (int i = 2; i<x; i++) {
-        if (x % i) {
-            vsota += i + x / i;
+    int vsota = 1;
+    for (int i = 2; i<=sqrt(x); i++) {
+        if (x % i == 0) {
+            vsota += i;
+            if (x / i != i) {
+                vsota += x / i;
+            }
         }
     }
     
